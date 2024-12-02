a0b9daeefe8663540bc889506fd4f9bf0f4807ef
Carson Buttars <carsonbuttars13@gmail.com>
2024-12-02 09:42:02 -0700

    Make a prophet model submission

--- submission.R
+++ submission.R
@@ -6,7 +6,7 @@ library(stacks)
 library(doParallel)
 
 # find number of cores
-num_cores <- parallel::detectCores()
+num_cores <- 4
 
 # Additional data
 inflation <- c(1.6, 2.0, 1.5, 1.1, 1.4, 1.8, 2.0, 1.5, 1.2, 1.0, 1.2, 1.5,
@@ -22,61 +22,18 @@ inf_df <- tibble(date = seq(as.Date("2013/1/1"),
   mutate(year = year(date)) %>%
   mutate(month = month(date)) %>%
   select(!date)
-holidays <- c("ChristmasDay",
-              "ChristmasEve",
-              "Easter",
-              "EasterMonday",
-              "EasterSunday",
-              "LaborDay",
-              "NewYearsDay",
-              "DENewYearsEve",
-              "USColumbusDay",
-              "USIndependenceDay",
-              "USMemorialDay",
-              "USMLKingsBirthday",
-              "USThanksgivingDay")
 
 # Load in data
-train_dirty <- vroom("../input/demand-forecasting-kernels-only/train.csv") %>%
-  mutate(year = year(date),
-         month = month(date)) %>%
-  left_join(inf_df, by = c("year", "month")) %>%
-  select(!year) %>%
-  select(!month)
-test_dirty <- vroom("../input/demand-forecasting-kernels-only/test.csv") %>%
-  mutate(year = year(date),
-         month = month(date)) %>%
-  left_join(inf_df, by = c("year", "month")) %>%
-  select(!year) %>%
-  select(!month)
-
-# Create the recipe
-recipe <- recipe(sales ~ ., train_dirty) %>%
-  step_holiday(date, holidays = holidays) %>%
-  step_date(date, features = "decimal") %>%
-  step_date(date, features = "dow", label = FALSE) %>%
-  step_mutate(date_dow = factor(date_dow)) %>%
-  step_dummy(date_dow) %>%
-  step_mutate(sinYear = sin(date_decimal * pi * 2),
-              cosYear = cos(date_decimal * pi * 2)) %>%
-  step_rm(date, store, item) %>%
-  step_normalize(all_predictors())
+train_dirty <- vroom("train.csv") %>%
+  left_join(inf_df, by = c("year", "month"))
+test_dirty <- vroom("test.csv") %>%
+  left_join(inf_df, by = c("year", "month"))
 
 # Set up output
-input <- train_dirty
-input$pred <- 0
 output <- test_dirty
 output$sales <- 0
 
-# Create the model and workflow
-penalized_model <- linear_reg(penalty = tune(), mixture = tune()) %>%
-  set_engine("glmnet") %>%
-  set_mode("regression")
-penalized_workflow <- workflow() %>%
-  add_model(penalized_model) %>%
-  add_recipe(recipe)
-
-# Fit a penalized regression for each store-item combination
+# Fit a prophet regression for each store-item combination
 count <- 1
 stores <- 1:10
 items <- 1:50
@@ -89,43 +46,36 @@ for (store in stores) {
                            & (test_dirty$item == item), ]
 
     # Create folds
-    folds <- vfold_cv(tmp_train, v = 10, repeats = 1)
+    folds <- time_series_split(tmp_train,
+                               assess = "3 months",
+                               cumulative = TRUE)
+
+    # Create the model
+    prophet_model <- prophet_reg() %>%
+      set_engine("prophet") %>%
+      fit(sales ~ ., data = training(folds))
 
     # Tune
     cl <- makePSOCKcluster(num_cores)
     registerDoParallel(cl)
-    param_grid <- grid_regular(penalty(),
-                               mixture(),
-                               levels = 10)
-    penalized_cv <- penalized_workflow %>%
-      tune_grid(resamples = folds,
-                grid = param_grid,
-                metrics = metric_set(smape),
-                control = control_stack_grid())
-    best_tune <- penalized_cv %>%
-      select_best(metric = "smape")
+    prophet_cv %>%
+      modeltime_forecast(new_data = testing(folds),
+                         actual_data = training(folds))
     stopCluster(cl)
 
     # Fit model and make predictions
-    penalized_fit <- penalized_workflow %>%
-      finalize_workflow(best_tune) %>%
-      fit(data = tmp_train)
-    penalized_predictions <- predict(penalized_fit,
-                                     new_data = tmp_test)$.pred
-    train_predictions <- predict(penalized_fit,
-                                 new_data = tmp_train)$.pred
-
-    # Put training predictions in the original dataset
-    input[(input$store == store)
-          & (input$item == item), "pred"] <- train_predictions
+    prophet_fit <- prophet_cv %>%
+      modeltime_refit(data = tmp_train)
+    prophet_predictions <- predict(prophet_fit,
+                                   new_data = tmp_test)$.pred
 
     # Put predictions in output data frame
     output[(output$store == store)
-           & (output$item == item), "sales"] <- penalized_predictions
+           & (output$item == item), "sales"] <- prophet_predictions
 
     # Print to monitor progress
     paste("Store: ", store, "\t",
-          "Item: ", item, "\t",
+          "Item: ", item, "   \t",
           "Count: ", count, "/500", sep = "") %>%
       write(stdout())
     count <- count + 1
