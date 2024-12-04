2a1d33b58743eb8808d664f80a7ffeab1c4437e1
Carson Buttars <carsonbuttars13@gmail.com>
2024-12-04 08:57:38 -0700

    Attempt without inflation data

--- submission.R
+++ submission.R
@@ -10,34 +10,9 @@ library(timetk)
 # find number of cores
 num_cores <- 4
 
-# Additional data
-inflation <- c(1.6, 2.0, 1.5, 1.1, 1.4, 1.8, 2.0, 1.5, 1.2, 1.0, 1.2, 1.5,
-               1.6, 1.1, 1.5, 2.0, 2.1, 2.1, 2.0, 1.7, 1.7, 1.7, 1.3, 0.8,
-               -0.1, 0.0, -0.1, -0.2, 0.0, 0.1, 0.2, 0.2, 0.0, 0.2, 0.5, 0.7,
-               1.4, 1.0, 0.9, 1.1, 1.0, 1.0, 0.8, 1.1, 1.5, 1.6, 1.7, 2.1,
-               2.5, 2.7, 2.4, 2.2, 1.9, 1.6, 1.7, 1.9, 2.2, 2.0, 2.2, 2.1,
-               2.1, 2.2, 2.4)
-inf_df <- tibble(date = seq(as.Date("2013/1/1"),
-                            as.Date("2018/3/1"),
-                            "month"),
-                 rate = inflation) %>%
-  mutate(year = year(date)) %>%
-  mutate(month = month(date)) %>%
-  select(!date)
-
 # Load in data
-train_dirty <- vroom("../input/demand-forecasting-kernels-only/train.csv") %>%
-  mutate(year = year(date),
-         month = month(date)) %>%
-  left_join(inf_df, by = c("year", "month")) %>%
-  select(!year) %>%
-  select(!month)
-test_dirty <- vroom("../input/demand-forecasting-kernels-only/test.csv") %>%
-  mutate(year = year(date),
-         month = month(date)) %>%
-  left_join(inf_df, by = c("year", "month")) %>%
-  select(!year) %>%
-  select(!month)
+train_dirty <- vroom("../input/demand-forecasting-kernels-only/train.csv")
+test_dirty <- vroom("../input/demand-forecasting-kernels-only/test.csv")
 
 # Set up output
 output <- test_dirty
